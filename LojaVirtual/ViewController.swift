560ea0b80577f306abbbfa203fbdf857e1d164a6
Bruno Fonseca <btfonseca@gmail.com>
2020-10-02 20:13:03 -0300

    criando o projeto

---
+++ LojaVirtual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LojaVirtual
+//
+//  Created by Bruno da Fonseca on 02/10/20.
+//  Copyright © 2020 Bruno Fonseca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
